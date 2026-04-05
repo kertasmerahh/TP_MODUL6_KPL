e09a09ede7101952ca6aa4df4707e10fde576a23
kertasmerahh <yosuawibowo151@gmail.com>
2026-04-06 05:05:15 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-
 public class SayaMusicTrack
 {
     private int id;
@@ -9,10 +7,6 @@ public class SayaMusicTrack
 
     public SayaMusicTrack(string title)
     {
-
-        Debug.Assert(title != null, "Title tidak boleh null!");
-        Debug.Assert(title.Length <= 100, "Title maksimal 100 karakter!");
-
         Random rnd = new Random();
         this.id = rnd.Next(10000, 99999);
         this.title = title;
@@ -21,21 +15,7 @@ public class SayaMusicTrack
 
     public void IncreasePlayCount(int count)
     {
-
-        Debug.Assert(count <= 10000000, "Input penambahan maksimal 10.000.000!");
-
-        try
-        {
-
-            checked
-            {
-                this.playCount += count;
-            }
-        }
-        catch (OverflowException)
-        {
-            Console.WriteLine("Error: Play count sudah melebihi batas maksimal integer!");
-        }
+        this.playCount += count;
     }
 
     public void PrintTrackDetails()
@@ -43,26 +23,14 @@ public class SayaMusicTrack
         Console.WriteLine($"ID: {id}");
         Console.WriteLine($"Title: {title}");
         Console.WriteLine($"Play Count: {playCount}");
-        Console.WriteLine("-------------------------");
     }
 }
-
 class Program
 {
     static void Main(string[] args)
     {
-
-        SayaMusicTrack lagu1 = new SayaMusicTrack("Hanya Memuji");
-        lagu1.IncreasePlayCount(1000);
-        lagu1.PrintTrackDetails();
-
-        lagu1.IncreasePlayCount(11000000);
-
-        Console.WriteLine("Testing Overflow...");
-        for (int i = 0; i < 300; i++)
-        {
-            lagu1.IncreasePlayCount(10000000);
-        }
-        lagu1.PrintTrackDetails();
+        SayaMusicTrack lagu = new SayaMusicTrack("Lagu TP KPL");
+        lagu.IncreasePlayCount(100);
+        lagu.PrintTrackDetails();
     }
 }
